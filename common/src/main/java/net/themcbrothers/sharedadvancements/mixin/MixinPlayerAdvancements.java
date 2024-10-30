de28a4dfddb0302a299c0743de0524fc39bd0f66
TheMCLoveMan <themcloveman@gmail.com>
2024-10-30 20:55:37 +0100

    PlayerList mixin - reduces mod loader dependency

--- common/src/main/java/net/themcbrothers/sharedadvancements/mixin/MixinPlayerAdvancements.java
+++ common/src/main/java/net/themcbrothers/sharedadvancements/mixin/MixinPlayerAdvancements.java
@@ -1,25 +1,22 @@
 package net.themcbrothers.sharedadvancements.mixin;
 
-import net.themcbrothers.sharedadvancements.CommonClass;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.server.PlayerAdvancements;
 import net.minecraft.server.level.ServerPlayer;
+import net.themcbrothers.sharedadvancements.CommonClass;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PlayerAdvancements.class)
 public class MixinPlayerAdvancements {
     @Shadow
     private ServerPlayer player;
 
-    @Inject(method = "award", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private void award(Advancement advancement, String criterionKey, CallbackInfoReturnable<Boolean> cir, boolean success) {
-        if (success) {
-            CommonClass.progressAdvancement(player, criterionKey, advancement);
-        }
+    @Inject(method = "award", at = @At("TAIL"))
+    private void award(Advancement advancement, String criterionKey, CallbackInfoReturnable<Boolean> cir) {
+        CommonClass.progressAdvancement(player, criterionKey, advancement);
     }
 }
---
+++ common/src/main/java/net/themcbrothers/sharedadvancements/mixin/MixinPlayerList.java
@@ -0,0 +1,18 @@
+package net.themcbrothers.sharedadvancements.mixin;
+
+import net.minecraft.network.Connection;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import net.themcbrothers.sharedadvancements.CommonClass;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerList.class)
+public class MixinPlayerList {
+    @Inject(method = "placeNewPlayer", at = @At("TAIL"))
+    private void placeNewPlayer(Connection connection, ServerPlayer serverPlayer, CallbackInfo ci) {
+        CommonClass.playerJoin(serverPlayer);
+    }
+}
