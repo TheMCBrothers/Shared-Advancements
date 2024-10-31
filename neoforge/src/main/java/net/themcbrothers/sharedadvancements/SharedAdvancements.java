d0a77b30a180b2240673c6c7e864d51b19716be4
TheMCLoveMan <themcloveman@gmail.com>
2024-10-31 02:10:28 +0100

    fix neoforge project

--- neoforge/src/main/java/net/themcbrothers/sharedadvancements/SharedAdvancements.java
+++ neoforge/src/main/java/net/themcbrothers/sharedadvancements/SharedAdvancements.java
@@ -1,11 +1,12 @@
 package net.themcbrothers.sharedadvancements;
 
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 
 @Mod(Constants.MOD_ID)
 public class SharedAdvancements {
-    public SharedAdvancements() {
+    public SharedAdvancements(ModContainer modContainer) {
         CommonClass.init();
         modContainer.registerConfig(ModConfig.Type.SERVER, SharedAdvancementsConfig.SPEC);
     }
