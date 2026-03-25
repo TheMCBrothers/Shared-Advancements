bf7161b5369ca9cb6dd000ffde2d8512d0cb1360
TheMCLoveMan <68924107+TheMCLoveMan@users.noreply.github.com>
2026-03-25 13:47:37 +0100

    Forge Support MC 26.1

--- common/src/main/java/net/themcbrothers/sharedadvancements/mixin/MixinPlayerAdvancements.java
+++ common/src/main/java/net/themcbrothers/sharedadvancements/mixin/MixinPlayerAdvancements.java
@@ -1,25 +1,24 @@
 package net.themcbrothers.sharedadvancements.mixin;
 
-import net.themcbrothers.sharedadvancements.CommonClass;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.server.PlayerAdvancements;
 import net.minecraft.server.level.ServerPlayer;
+import net.themcbrothers.sharedadvancements.CommonClass;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PlayerAdvancements.class)
 public class MixinPlayerAdvancements {
     @Shadow
     private ServerPlayer player;
 
-    @Inject(method = "award", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private void award(AdvancementHolder advancement, String criterionKey, CallbackInfoReturnable<Boolean> cir, boolean success) {
-        if (success) {
-            CommonClass.progressAdvancement(player, criterionKey, advancement);
+    @Inject(method = "award", at = @At("TAIL"))
+    private void award(AdvancementHolder holder, String criterion, CallbackInfoReturnable<Boolean> cir) {
+        if (cir.getReturnValue()) {
+            CommonClass.progressAdvancement(player, criterion, holder);
         }
     }
 }
